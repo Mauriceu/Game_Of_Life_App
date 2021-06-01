87ee7731b1b74bfae1793d67bc4ce4d33d186c88
Ramin Adib <raminadib@kpmg.com>
2021-06-01 16:48:28 +0200

    Funktion test Angellegt.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -55,6 +55,7 @@ namespace Game_Of_Life_App
             
             // activate buttons
             ButtonStart.IsEnabled = true;
+            MAX_LIVING_STARTCELLS = _numberRows * _numberColumns;//
             InputStartCells.IsEnabled = true;
             ButtonRandomize.IsEnabled = true;
         }
